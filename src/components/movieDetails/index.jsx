56506c525cd34cd104d877f3dd52bce89ae1ad48
Matul Jain <matul3jan@gmail.com>
2023-02-08 19:24:35 +0000

    Movie Details Page - included production countries

--- src/components/movieDetails/index.jsx
+++ src/components/movieDetails/index.jsx
@@ -44,6 +44,7 @@ const MovieDetails = (props) => {
           </li>
         ))}
       </Paper>
+
       <Paper component="ul" sx={styles.chipSet}>
         <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
         <Chip
@@ -55,6 +56,21 @@ const MovieDetails = (props) => {
           label={`${movie.vote_average} (${movie.vote_count}`}
         />
         <Chip label={`Released: ${movie.release_date}`} />
+      </Paper>
+
+      <Paper component="ul" sx={styles.chipSet}>
+        <li>
+          <Chip
+            label="Production Countries"
+            sx={styles.chipLabel}
+            color="primary"
+          />
+        </li>
+        {movie.production_countries.map((g) => (
+          <li key={g.name}>
+            <Chip label={g.name} />
+          </li>
+        ))}
       </Paper>
     </>
   );
