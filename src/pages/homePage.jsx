200391ba2f6072037db29681336d2aa959e97d0e
Matul Jain <matul3jan@gmail.com>
2023-02-14 10:28:06 +0000

    Add to favourites logic completed; Dummy favourites page

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -39,6 +39,13 @@ const MovieListPage = (props) => {
     else setGenreFilter(value);
   };
 
+  const addToFavourites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favourite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${
@@ -63,7 +70,10 @@ const MovieListPage = (props) => {
           <Header title={"Home Page"} />
         </Grid>
         <Grid item container spacing={5}>
-          <MovieList movies={displayedMovies}></MovieList>
+          <MovieList
+            movies={displayedMovies}
+            selectFavourite={addToFavourites}
+          />
         </Grid>
       </Grid>
       <Fab
