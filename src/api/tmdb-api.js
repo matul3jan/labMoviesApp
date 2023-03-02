6042aa27aaf6e5852ebfa157a6e6b934906285b5
Matul Jain <matul3jan@gmail.com>
2023-03-02 16:05:16 +0000

    Show movie review excerpts; Refactor Details page

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -23,3 +23,10 @@ export const getMovieImages = id => fetch(
     .then((res) => res.json())
     .then((json) => json.posters);
 
+export const getMovieReviews = (id) => {
+    return fetch(
+        `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${import.meta.env.VITE_TMDB_KEY}`
+    )
+        .then((res) => res.json())
+        .then((json) => json.results);
+};
--- src/components/movieDetails/index.jsx
+++ src/components/movieDetails/index.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Chip from "@mui/material/Chip";
 import Paper from "@mui/material/Paper";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import MonetizationIcon from "@mui/icons-material/MonetizationOn";
 import StarRate from "@mui/icons-material/StarRate";
 import Typography from "@mui/material/Typography";
+
+import NavigationIcon from "@mui/icons-material/Navigation";
+import Fab from "@mui/material/Fab";
+import Drawer from "@mui/material/Drawer";
+import MovieReviews from "../movieReviews";
 
 const styles = {
   chipSet: {
@@ -19,10 +24,15 @@ const styles = {
   chipLabel: {
     margin: 0.5,
   },
+  fab: {
+    position: "fixed",
+    top: 50,
+    right: 2,
+  },
 };
 
-const MovieDetails = (props) => {
-  const movie = props.movie;
+const MovieDetails = ({ movie }) => {
+  const [drawerOpen, setDrawerOpen] = useState(false); // New
 
   return (
     <>
@@ -44,7 +54,6 @@ const MovieDetails = (props) => {
           </li>
         ))}
       </Paper>
-
       <Paper component="ul" sx={styles.chipSet}>
         <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
         <Chip
@@ -57,22 +66,24 @@ const MovieDetails = (props) => {
         />
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
-
-      <Paper component="ul" sx={styles.chipSet}>
-        <li>
-          <Chip
-            label="Production Countries"
-            sx={styles.chipLabel}
-            color="primary"
-          />
-        </li>
-        {movie.production_countries.map((g) => (
-          <li key={g.name}>
-            <Chip label={g.name} />
-          </li>
-        ))}
-      </Paper>
+      <Fab
+        color="secondary"
+        variant="extended"
+        onClick={() => setDrawerOpen(true)}
+        sx={styles.fab}
+      >
+        <NavigationIcon />
+        Reviews
+      </Fab>
+      <Drawer
+        anchor="top"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <MovieReviews movie={movie} />
+      </Drawer>
     </>
   );
 };
+
 export default MovieDetails;
