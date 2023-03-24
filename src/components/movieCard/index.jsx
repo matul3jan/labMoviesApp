ce7fc4989456160d15b2c3f9cff77883fae82874
Matul Jain <matul3jan@gmail.com>
2023-03-24 10:18:39 +0000

    Refactor: Favourite movie list now stored in a context

--- src/components/movieCard/index.jsx
+++ src/components/movieCard/index.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -12,8 +13,10 @@ import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
 import StarRateIcon from "@mui/icons-material/StarRate";
 import Grid from "@mui/material/Grid";
 import IconButton from "@mui/material/IconButton";
-import img from "../../images/film-poster-placeholder.png";
 import Avatar from "@mui/material/Avatar";
+
+import { MoviesContext } from "../../contexts/moviesContext";
+import img from "../../images/film-poster-placeholder.png";
 
 const styles = {
   card: { maxWidth: 345 },
@@ -23,12 +26,18 @@ const styles = {
   },
 };
 
-export default function MovieCard(props) {
-  const movie = props.movie;
+export default function MovieCard({ movie }) {
+  const { favourites, addToFavourites } = useContext(MoviesContext);
+
+  if (favourites.find((id) => id === movie.id)) {
+    movie.favourite = true;
+  } else {
+    movie.favourite = false;
+  }
 
   const handleAddToFavourite = (e) => {
     e.preventDefault();
-    props.selectFavourite(movie.id);
+    addToFavourites(movie);
   };
 
   return (
--- src/pages/favouriteMoviesPage.jsx
+++ src/pages/favouriteMoviesPage.jsx
@@ -1,27 +1,56 @@
-import React from "react";
+import React, { useContext } from "react";
 import PageTemplate from "../components/templateMovieListPage";
+import { MoviesContext } from "../contexts/moviesContext";
+import { useQueries } from "react-query";
+import { getMovie } from "../api/tmdb-api";
+import Spinner from "../components/spinner";
 import useFiltering from "../hooks/useFiltering";
-import MovieFilterUI, {
-  titleFilter,
-  genreFilter,
-} from "../components/movieFilterUI";
+import MovieFilterUI, { titleFilter } from "../components/movieFilterUI";
 
 const titleFiltering = {
   name: "title",
   value: "",
   condition: titleFilter,
 };
-const genreFiltering = {
+export const genreFiltering = {
   name: "genre",
   value: "0",
-  condition: genreFilter,
+  condition: function (movie, value) {
+    // Is user selected genre in this movies's genre list?
+    // Always true if selected genre ia All (0).
+    const genreId = Number(value);
+    const genre_ids = movie.genres.map((g) => g.id);
+    return genreId > 0 ? genre_ids.includes(genreId) : true;
+  },
 };
 
-const FavouriteMoviesPage = (props) => {
+const FavouriteMoviesPage = () => {
+  const { favourites: movieIds } = useContext(MoviesContext);
   const { filterValues, setFilterValues, filterFunction } = useFiltering(
     [],
     [titleFiltering, genreFiltering]
   );
+
+  // Create an array of queries and run them in parallel.
+  const favouriteMovieQueries = useQueries(
+    movieIds.map((movieId) => {
+      return {
+        queryKey: ["movie", { id: movieId }],
+        queryFn: getMovie,
+      };
+    })
+  );
+  // Check if any of the parallel queries is still loading.
+  const isLoading = favouriteMovieQueries.find((m) => m.isLoading === true);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  const allFavourites = favouriteMovieQueries.map((q) => q.data);
+  const displayMovies = allFavourites ? filterFunction(allFavourites) : [];
+
+  const toDo = () => true;
 
   const changeFilterValues = (type, value) => {
     const changedFilter = { name: type, value: value };
@@ -32,17 +61,11 @@ const FavouriteMoviesPage = (props) => {
     setFilterValues(updatedFilterSet);
   };
 
-  const favouriteMovies = JSON.parse(localStorage.getItem("favourites"));
-
-  const displayedMovies = filterFunction(favouriteMovies);
-
-  const toDo = () => true;
-
   return (
     <>
       <PageTemplate
         title="Favourite Movies"
-        movies={displayedMovies}
+        movies={displayMovies}
         selectFavourite={toDo}
       />
       <MovieFilterUI
